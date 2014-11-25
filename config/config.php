75016b708eda9a6ac366cf99c4619d3cc84bb0ce
xiaonianiu <liuguangpingtest@163.com>
2014-11-25 20:30:17 +0800

    first commit

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright (C) "Code Generator Tools"
+ * @author liuguangpingAuthor liuguangpingtest@163.com
+ */
+
+return array(
+	'db_host' => 'localhost',
+	'db_user' => 'root',
+	'db_password' => '123456',
+	'db_name' => 'tpgy',
+	'db_charset' => 'utf8_general_ci'
+);
+
+?>
